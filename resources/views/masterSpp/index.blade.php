b32e4c058b941cdb9bcfe803301581cea1d0ea75
justputoff <kholid.hasibuan35@gmail.com>
2024-07-29 17:17:31 +0700

    c
    c

--- resources/views/masterSpp/index.blade.php
+++ resources/views/masterSpp/index.blade.php
@@ -6,31 +6,31 @@
 
 <div class="container-xxl flex-grow-1 container-p-y">
   <div class="card">
-    <h5 class="card-header">Tambah SPP</h5>
+    <h5 class="card-header">Tambah Master Nominal</h5>
     <form action="{{ route('spp/student.store') }}" method="POST" class="p-3" enctype="multipart/form-data">
       @csrf
       <div class="row mb-3">
-        <label class="col-sm-2 col-form-label" for="basic-default-name">Name</label>
+        <label class="col-sm-2 col-form-label" for="basic-default-name">Label</label>
         <div class="col-sm-10">
           <input type="text" name="name" class="form-control" id="basic-default-name" placeholder="" />
         </div>
       </div>
       <div class="row mb-3">
-        <label class="col-sm-2 col-form-label" for="basic-default-name">Tarif SPP</label>
+        <label class="col-sm-2 col-form-label" for="basic-default-name">Tarif Master Nominal</label>
         <div class="col-sm-10">
           <input type="number" name="price" class="form-control" id="basic-default-name" placeholder="" />
         </div>
       </div>
       <div class="row justify-content-end">
         <div class="col-sm-10">
-          <button type="submit" onclick="return confirm('Apakah Anda yakin ingin mengirim data ini?')" class="btn btn-sm btn-dark mt-3">Kirim</button>
+          <button type="submit" onclick="return confirm('Apakah Anda yakin ingin mengirim data ini?')" class="btn btn-sm btn-dark mt-3">Simpan</button>
         </div>
       </div>
     </form>
   </div>
 
   <div class="card mt-3">
-    <h5 class="card-header">Table SPP</h5>
+    <h5 class="card-header">Table Master Nominal</h5>
     <div class="table-responsive text-nowrap p-3">
       <table class="table" id="example">
         <thead>
