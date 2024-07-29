ce3fc2eb1cd1d3914bfcfd2fc72660b5fcdef566
justputoff <kholid.hasibuan35@gmail.com>
2024-07-29 16:46:14 +0700

    c
    c

--- routes/auth.php
+++ routes/auth.php
@@ -20,6 +20,13 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
+    Route::get('/', [AuthenticatedSessionController::class, 'create'])
+                ->name('welcome');
+
+    // Route::get('/', function () {
+    //     return view('welcome');
+    // });
+
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
